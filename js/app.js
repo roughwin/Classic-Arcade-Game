938d2ef36835919189d499a7b07f34c0a12af11f
roughwin <roughwin@qq.com>
2017-03-03 22:16:45 +0800

    主体完成

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
+var boy_area = [1000,1000,83,89]
 // 这是我们的玩家要躲避的敌人 
-var Enemy = function() {
+var Enemy = function(pos) {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
-
+    this.x = pos.x;//-100;
+    this.y = pos.y;//65;
     // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
 };
@@ -12,6 +14,13 @@ var Enemy = function() {
 Enemy.prototype.update = function(dt) {
     // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
+    if(this.x > 580) {
+        this.x = -100;
+    }       
+    this.x += dt * 200;
+    if(this.x + 100 >= boy_area[0] && this.x <= boy_area[2] && Math.abs(this.y - boy_area[1]) < 20)
+        console.log('hit')
+    this.render();
 };
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
@@ -21,13 +30,48 @@ Enemy.prototype.render = function() {
 
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
-
-
+var Player = function(pos) {
+    this.sprite = 'images/char-boy.png';
+    this.x = pos.x;
+    this.y = pos.y;
+}
+Player.prototype.update = function(dt) {
+    boy_area[0] = 17 + this.x
+    boy_area[2] = 83 + this.x
+    boy_area[1] = 10 + this.y
+}
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+Player.prototype.handleInput = function(keys) {
+    // console.log(keys);
+    switch(keys) {
+        case 'left':
+            this.x = this.x < 100 ? this.x : this.x - 100;
+            break;
+        case 'right':
+            this.x = this.x > 399 ? this.x : this.x + 100;
+            break;
+        case 'up':
+            this.y = this.y < 0 ? this.y : this.y - 84;
+            break;
+        case 'down':
+            this.y = this.y > 400 ? this.y : this.y + 84;
+            break;
+        default:
+            break;
+    }
+}
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
+
+var allEnemies = [new Enemy({x:-100, y:65})]
 // 把玩家对象放进一个叫 player 的变量里面
 
-
+var player = new Player({
+    x: 200,
+    y: 410
+})
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
 // 方法里面。你不需要再更改这段代码了。
 document.addEventListener('keyup', function(e) {
